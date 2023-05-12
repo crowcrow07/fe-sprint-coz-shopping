e6cd0584819fa6b79bfd76c9f0ef5bf81b89ac35
crowcrow07 <skathd3402@gmail.com>
2023-05-12 14:07:18 +0900

    feat : 라우팅 작업 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Main from "./Page/Main";
+import ProductsList from "./Page/ProductsList";
+import BookMark from "./Page/BookMark";
+
 import "./App.css";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      <div className="flex">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/products/list" element={<ProductsList />} />
+          <Route path="/bookmark" element={<BookMark />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
