a75535646327b66ed83ca590bab7efa27689ab36
crowcrow07 <skathd3402@gmail.com>
2023-05-12 14:41:13 +0900

    feat : Footer import 및 잘못된 경로 진입시 없는 페이지 띄우기 구현

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import Header from "./components/Header";
 import Main from "./Page/Main";
 import ProductsList from "./Page/ProductsList";
 import BookMark from "./Page/BookMark";
+import Footer from "./components/Footer";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="flex">
+      <div className="flex flex-col">
         <Header />
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/products/list" element={<ProductsList />} />
           <Route path="/bookmark" element={<BookMark />} />
+          <Route path="*" element={<div>없는페이지임</div>} />
         </Routes>
+        <Footer />
       </div>
     </div>
   );
