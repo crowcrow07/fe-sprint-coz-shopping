cc24fe9e396414a21eb7abbf366de9e3fa64715e
crowcrow07 <skathd3402@gmail.com>
2023-05-15 15:00:17 +0900

    test : product Card 테스트 진행중

--- src/Page/Main.js
+++ src/Page/Main.js
@@ -1,3 +1,23 @@
+import { useState, useEffect } from "react";
+
+import ProductCard from "../components/ProductCard";
+import Products from "../api/Products";
+
 export default function Main() {
-  return <div>메인페이지</div>;
+  const [productsDataes, setProductsDataes] = useState(null);
+  useEffect(() => {
+    Products.getFourProducts()
+      .then((res) => res.json())
+      .then((json) => {
+        setProductsDataes(json);
+      });
+  }, []);
+  return (
+    <div className="flex">
+      {productsDataes &&
+        productsDataes.map((data) => {
+          return <ProductCard productsData={data} />;
+        })}
+    </div>
+  );
 }
