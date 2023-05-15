e7c2a51205c579e91a2b8571f8b609f7901976b9
crowcrow07 <skathd3402@gmail.com>
2023-05-15 13:39:02 +0900

    feat : api호출을 위한 페이지 구현

---
+++ src/api/Products.js
@@ -0,0 +1,12 @@
+const API_BASE = "http://cozshopping.codestates-seb.link/api/v1/products";
+
+export default {
+  getAllProducts() {
+    const allProducts = fetch({ API_BASE });
+    return allProducts;
+  },
+  getFourProducts() {
+    const fourProducts = fetch(`${API_BASE}?count=4`);
+    return fourProducts;
+  },
+};
