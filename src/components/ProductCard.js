e9168ca7e500d28bdfe40c342e0141df985f7683
crowcrow07 <skathd3402@gmail.com>
2023-05-15 17:24:14 +0900

    feat : 상품카드 제작완료

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,20 +1,41 @@
 export default function ProductCard({ productsData }) {
-  //타입이 뭔지 검사한다.
-  //타입이 Product라면 title과 price, discountPercentage가 있어야함
-  //타입이 Category라면 title만 있으면됨
-  //타입이 Exhibition라면 title과 sub_title이 있어야함
-  //타입이 Brand라면 brand_name과 follower가 있어야함
-  console.log(productsData);
+  const type = productsData.type;
+  const imgUrl = productsData.image_url;
+  const brandImgUrl = productsData.brand_image_url;
+  const title = productsData.title;
+  const subTitle = productsData.sub_title;
+  const brandName = productsData.brand_name;
+  const price = productsData.price;
+  const discountPercentage = productsData.discountPercentage;
+  const follower = productsData.follower;
+
   return (
-    <div className="flex-col w-264 h-264 border">
+    <div className="flex flex-col w-264 h-264 border">
       <div className="h-4/5">
         <img
-          className="h-full"
-          src="https://images.unsplash.com/photo-1626372416494-3183a0c4f726?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2012&q=80"
+          className="h-full w-full"
+          src={brandImgUrl ? brandImgUrl : imgUrl}
           alt="상품이미지"
         />
       </div>
-      <div className="h-1/5">상품설명공간</div>
+      <div className="flex flex-col h-1/5">
+        <div className="flex flex-row justify-between">
+          <div>
+            {type === "Category" ? `#${title}` : title ? title : brandName}
+          </div>
+          <div className={discountPercentage ? "text-violet-600" : ""}>
+            {discountPercentage
+              ? `${discountPercentage}%`
+              : type === "Brand"
+              ? "관심고객수"
+              : null}
+          </div>
+        </div>
+        <div className="flex flex-row justify-between">
+          <div>{subTitle}</div>
+          <div>{price ? (+price).toLocaleString() : follower}</div>
+        </div>
+      </div>
     </div>
   );
 }
