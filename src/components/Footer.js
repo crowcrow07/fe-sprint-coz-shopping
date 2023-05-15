47e7bf3533bbd88100cc1f597a2777c3dafca880
crowcrow07 <skathd3402@gmail.com>
2023-05-15 13:48:18 +0900

    feat : footer 글씨색 변환

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="flex justify-center items-center text-center w-screen h-58 border">
+    <div className="flex justify-center items-center text-center w-screen h-58 border text-stone-500">
       개인정보 처리방침 | 이용 약관
       <br />
       All rights reserved @ Codestates
