e12835799122a870a9b736e9ee370ea1da7e8a94
crowcrow07 <skathd3402@gmail.com>
2023-05-16 16:57:47 +0900

    feat : 북마크리스트 출력 카드에 props 전달

--- src/Page/Main.js
+++ src/Page/Main.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import ProductCard from "../components/ProductCard";
 import Products from "../api/Products";
 
-export default function Main() {
+export default function Main({ localDataes, setLocalDataes }) {
   const [productsDataes, setProductsDataes] = useState(null);
   useEffect(() => {
     Products.getFourProducts()
@@ -13,11 +13,37 @@ export default function Main() {
       });
   }, []);
   return (
-    <div className="flex">
-      {productsDataes &&
-        productsDataes.map((data) => {
-          return <ProductCard productsData={data} />;
-        })}
+    <div className="flex flex-col w-5/6">
+      <div className="m-2 font-bold text-2xl">상품 리스트</div>
+      <div className="flex flex-row justify-between">
+        {productsDataes &&
+          productsDataes.map((data) => {
+            return (
+              <ProductCard
+                key={data.id}
+                productsData={data}
+                localDataes={localDataes}
+                setLocalDataes={setLocalDataes}
+              />
+            );
+          })}
+      </div>
+      <div>
+        <div className="m-2 font-bold text-2xl">북마크 리스트</div>
+        <div className="flex flex-row justify-between">
+          {localDataes &&
+            localDataes.map((data) => {
+              return (
+                <ProductCard
+                  key={localDataes.id}
+                  productsData={data}
+                  localDataes={localDataes}
+                  setLocalDataes={setLocalDataes}
+                />
+              );
+            })}
+        </div>
+      </div>
     </div>
   );
 }
