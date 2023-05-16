8999ba0f9339a350b9a07aeda8434c3f42ccdd19
crowcrow07 <skathd3402@gmail.com>
2023-05-16 21:55:06 +0900

    refactor : 삼품카드 리팩토링을 진행했습니다

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -7,51 +7,19 @@ export default function ProductCard({
   localDataes,
   setLocalDataes,
 }) {
-  const type = productsData.type;
   const imgUrl = productsData.image_url;
   const brandImgUrl = productsData.brand_image_url;
-  const title = productsData.title;
-  const subTitle = productsData.sub_title;
-  const brandName = productsData.brand_name;
-  const price = productsData.price;
-  const discountPercentage = productsData.discountPercentage;
-  const follower = productsData.follower;
 
-  const [onClickBookMark, setOnClickBookMark] = useState(false);
-
-  const renderTitle = () => {
-    switch (type) {
-      case "Category":
-        return `#${title}`;
-      case "Product":
-        return title;
-      case "Brand":
-        return brandName;
-      case "Exhibition":
-        return title;
-      default:
-        return null;
-    }
-  };
-
-  const renderPrice = () => {
-    if (!price) {
-      return follower;
-    }
-    return (+price).toLocaleString();
-  };
+  const [isBookmarked, setIsBookmarked] = useState(false);
 
   const onClickHandler = () => {
-    setOnClickBookMark(!onClickBookMark);
+    setIsBookmarked(!isBookmarked);
     if (
       localDataes.findIndex((localData) => localData.id === productsData.id) ===
       -1
     ) {
       setLocalDataes([...localDataes, productsData]);
-    } else if (
-      localDataes.findIndex((localData) => localData.id === productsData.id) !==
-      -1
-    ) {
+    } else {
       setLocalDataes(
         localDataes.filter((localData) => {
           return localData.id !== productsData.id;
@@ -63,36 +31,89 @@ export default function ProductCard({
   return (
     <div className="flex relative flex-col w-264 h-264">
       <div className="h-4/5">
-        <div className="absolute bottom-14 right-2" onClick={onClickHandler}>
-          <StarIcon
-            className="drop-shadow-lg"
-            sx={{
-              color: onClickBookMark ? "#FFD361" : "#DFDFDF",
-              fontSize: 40,
-            }}
-          />
-        </div>
         <img
           className="h-full w-full rounded-lg"
           src={brandImgUrl ? brandImgUrl : imgUrl}
           alt="상품이미지"
         />
+        <div className="absolute bottom-14 right-2" onClick={onClickHandler}>
+          <StarIcon
+            className="drop-shadow-lg"
+            sx={{
+              color: isBookmarked ? "#FFD361" : "#DFDFDF",
+              fontSize: 40,
+            }}
+          />
+        </div>
       </div>
+
       <div className="flex flex-col h-1/5">
-        <div className="flex flex-row justify-between">
-          <div className="font-bold">{renderTitle()}</div>
-          <div className={discountPercentage ? "text-violet-600" : ""}>
-            {discountPercentage
-              ? `${discountPercentage}%`
-              : type === "Brand"
-              ? "관심고객수"
-              : null}
-          </div>
+        <Bottom productsData={productsData} />
+      </div>
+    </div>
+  );
+}
+
+function Bottom({ productsData }) {
+  switch (productsData.type) {
+    case "Category":
+      return <CategoryBottom productsData={productsData} />;
+    case "Product":
+      return <ProductBottom productsData={productsData} />;
+    case "Brand":
+      return <BrandBottom productsData={productsData} />;
+    case "Exhibition":
+      return <ExhibitionBottom productsData={productsData} />;
+    default:
+      return null;
+  }
+}
+
+function ProductBottom({ productsData }) {
+  return (
+    <div className="w-full">
+      <div className="flex flex-row justify-between">
+        <div className="font-bold">{productsData.title}</div>
+        <div className="text-violet-600">
+          {productsData.discountPercentage}%
         </div>
-        <div className="flex flex-row justify-between">
-          <div>{subTitle}</div>
-          <div>{renderPrice()}</div>
-        </div>
+      </div>
+
+      <div className="flex flex-row justify-end">
+        <div>{(+productsData.price).toLocaleString()}원</div>
+      </div>
+    </div>
+  );
+}
+
+function CategoryBottom({ productsData }) {
+  return (
+    <div className="w-full">
+      <div className="font-bold"># {productsData.title}</div>
+    </div>
+  );
+}
+
+function ExhibitionBottom({ productsData }) {
+  return (
+    <div className="w-full">
+      <div className="flex flex-row justify-between">
+        <div className="font-bold">{productsData.title}</div>
+      </div>
+      <div className="flex">{productsData.sub_title}</div>
+    </div>
+  );
+}
+
+function BrandBottom({ productsData }) {
+  return (
+    <div>
+      <div className="flex flex-row justify-between">
+        <div className="font-bold">{productsData.brand_name}</div>
+        <div>관심고객수</div>
+      </div>
+      <div className="flex flex-row justify-end">
+        <div>{(+productsData.follower).toLocaleString()}명</div>
       </div>
     </div>
   );
