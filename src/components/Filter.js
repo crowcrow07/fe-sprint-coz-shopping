ab9b0f6562e477bf16d8e4036c4539135bfbba20
crowcrow07 <skathd3402@gmail.com>
2023-05-18 15:56:05 +0900

    feat : 필터 기능 css 완료

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,3 +1,48 @@
 export default function Filter() {
-  return <div className="h-5 border">필터입니다</div>;
+  return (
+    <div className="flex justify-center items-center w-screen h-32  m-4">
+      <div className="flex justify-between w-1/2">
+        <div className="flex flex-col items-center cursor-pointer">
+          <img
+            className="w-20 h-20  rounded-full"
+            src="/image/전체.jpeg"
+            alt="전체"
+          />
+          <div className="m-2">전체</div>
+        </div>
+        <div className="flex flex-col items-center cursor-pointer">
+          <img
+            className="w-20 h-20 rounded-full"
+            src="/image/상품.jpeg"
+            alt="상품"
+          />
+          <div className="m-2">상품</div>
+        </div>
+        <div className="flex flex-col items-center cursor-pointer">
+          <img
+            className="w-20 h-20 rounded-full"
+            src="/image/카테고리.jpeg"
+            alt="카테고리"
+          />
+          <div className="m-2">카테고리</div>
+        </div>
+        <div className="flex flex-col items-center cursor-pointer">
+          <img
+            className="w-20 h-20 rounded-full"
+            src="/image/기획전.jpeg"
+            alt="기획전"
+          />
+          <div className="m-2">기획전</div>
+        </div>
+        <div className="flex flex-col items-center cursor-pointer">
+          <img
+            className="w-20 h-20 rounded-full"
+            src="/image/브랜드.jpeg"
+            alt="브랜드"
+          />
+          <div className="m-2">브랜드</div>
+        </div>
+      </div>
+    </div>
+  );
 }
